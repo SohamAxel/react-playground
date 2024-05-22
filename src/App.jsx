594f648fa051af6ed961c6b498992853f47349a7
Soham Sengupta <soham.sengupta@axelerant.com>
2024-05-22 18:48:33 +0530

    Lazyloading using intersectionobserver

--- src/App.jsx
+++ src/App.jsx
@@ -1,83 +1,59 @@
 import { useRef } from "react";
 import "./styles.css";
 import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-  const card = useRef();
-
-  const observer = new IntersectionObserver(
-    (entries) => {
-      entries.forEach((entry) => {
-        entry.target.classList.toggle("show", entry.isIntersecting);
-        if (entry.isIntersecting) observer.unobserve(entry.target);
-      });
-    },
-    {
-      threshold: 1,
-    }
-  );
+  const lastCard = useRef();
+  const [datas, setDatas] = useState([
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+  ]);
 
   const lastElementObserver = new IntersectionObserver((entries) => {
     if (entries[0].isIntersecting) {
+      lastElementObserver.unobserve(lastCard.current);
       console.log("Reached Last card");
+      setDatas((currentData) => [
+        ...currentData,
+        ...[
+          currentData[currentData.length - 1] + 1,
+          currentData[currentData.length - 1] + 2,
+          currentData[currentData.length - 1] + 3,
+          currentData[currentData.length - 1] + 4,
+          currentData[currentData.length - 1] + 5,
+        ],
+      ]);
     }
   });
 
   useEffect(() => {
-    const cards = document.querySelectorAll(".card");
-    cards.forEach((element) => {
-      observer.observe(element);
-    });
-
-    lastElementObserver.observe(document.querySelector(".lastcard"));
-  }, []);
+    console.log("last card changed");
+    lastElementObserver.observe(lastCard.current);
+  }, [datas]);
 
   return (
     <>
       <h1>Hello</h1>
       <div className="card-container">
-        <div className="card show" ref={card}>
-          This is first card
-        </div>
-        <div className="card" ref={card}>
-          This is a card
-        </div>
-        <div className="card" ref={card}>
-          This is a card
-        </div>
-        <div className="card" ref={card}>
-          This is a card
-        </div>
-        <div className="card" ref={card}>
-          This is a card
-        </div>
-        <div className="card" ref={card}>
-          This is a card
-        </div>
-        <div className="card" ref={card}>
-          This is a card
-        </div>
-        <div className="card" ref={card}>
-          This is a card
-        </div>
-        <div className="card" ref={card}>
-          This is a card
-        </div>
-        <div className="card" ref={card}>
-          This is a card
-        </div>
-        <div className="card" ref={card}>
-          This is a card
-        </div>
-        <div className="card" ref={card}>
-          This is a card
-        </div>
-        <div className="card" ref={card}>
-          This is a card
-        </div>
-        <div className="card lastcard" ref={card}>
-          This is last card
-        </div>
+        {datas.map((data, index) => {
+          if (index == datas.length - 1) {
+            return (
+              <div
+                className="card show"
+                key={data}
+                ref={lastCard}
+                data-test="last"
+              >
+                This is card - {data}
+              </div>
+            );
+          }
+          return (
+            <div className="card show" key={data}>
+              This is card - {data}
+            </div>
+          );
+        })}
       </div>
     </>
   );
