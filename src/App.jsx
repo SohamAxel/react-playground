f7b4fc2185bdcba6a792b934baed0acb599e9a9d
Soham Sengupta <soham.sengupta@axelerant.com>
2024-05-22 19:36:35 +0530

    My attempt

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,64 @@
-import { useRef } from "react";
+import { useCallback, useRef } from "react";
 import "./styles.css";
 import { useEffect } from "react";
 import { useState } from "react";
+import { getPhotos } from "./apis/getPhotos";
+import { parseLinkHeader } from "./utility/parseLinkHeader";
 
 function App() {
-  const lastCard = useRef();
-  const [datas, setDatas] = useState([
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-  ]);
+  // const lastCard = useRef();
+  const [photos, setPhotos] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const nextPage = useRef();
+
+  const photoRef = useCallback((element) => {
+    if (element == null) return;
+    lastElementObserver.observe(element);
+  }, []);
 
   const lastElementObserver = new IntersectionObserver((entries) => {
     if (entries[0].isIntersecting) {
-      lastElementObserver.unobserve(lastCard.current);
-      console.log("Reached Last card");
-      setDatas((currentData) => [
-        ...currentData,
-        ...[
-          currentData[currentData.length - 1] + 1,
-          currentData[currentData.length - 1] + 2,
-          currentData[currentData.length - 1] + 3,
-          currentData[currentData.length - 1] + 4,
-          currentData[currentData.length - 1] + 5,
-        ],
-      ]);
+      lastElementObserver.unobserve(entries[0].target);
+      if (nextPage.current) {
+        startPhotoFetch(nextPage.current);
+      }
     }
   });
 
+  const startPhotoFetch = async (url) => {
+    setIsLoading(true);
+    const responseData = await fetch(url);
+    const data = await responseData.json();
+    const pagination = parseLinkHeader(responseData.headers.get("link"));
+    setPhotos((currPhotos) => [...currPhotos, ...data]);
+    nextPage.current = pagination.next;
+    setIsLoading(false);
+  };
+
   useEffect(() => {
-    console.log("last card changed");
-    lastElementObserver.observe(lastCard.current);
-  }, [datas]);
+    startPhotoFetch("http://127.0.0.1:3000/photos-short-list?_page=1&_limit=6");
+  }, []);
 
   return (
-    <>
-      <h1>Hello</h1>
-      <div className="card-container">
-        {datas.map((data, index) => {
-          if (index == datas.length - 1) {
-            return (
-              <div
-                className="card show"
-                key={data}
-                ref={lastCard}
-                data-test="last"
-              >
-                This is card - {data}
-              </div>
-            );
-          }
-          return (
-            <div className="card show" key={data}>
-              This is card - {data}
-            </div>
-          );
-        })}
-      </div>
-    </>
+    <div class="grid">
+      {photos.map((photo, index) => {
+        if (index == photos.length - 1) {
+          return <img src={photo.url} key={photo.id} ref={photoRef} />;
+        }
+        return <img src={photo.url} key={photo.id} />;
+      })}
+      {isLoading && (
+        <>
+          {" "}
+          <div className="skeleton">Loading...</div>
+          <div className="skeleton">Loading...</div>
+          <div className="skeleton">Loading...</div>
+          <div className="skeleton">Loading...</div>
+          <div className="skeleton">Loading...</div>
+          <div className="skeleton">Loading...</div>
+        </>
+      )}
+    </div>
   );
 }
 
