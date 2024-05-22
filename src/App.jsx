43c3ad16fb862e92746f57b44970600480a90a84
Soham Sengupta <soham.sengupta@axelerant.com>
2024-05-22 19:48:06 +0530

    Minor Modifications

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useCallback, useRef } from "react";
 import "./styles.css";
 import { useEffect } from "react";
 import { useState } from "react";
-import { getPhotos } from "./apis/getPhotos";
 import { parseLinkHeader } from "./utility/parseLinkHeader";
 
 function App() {
@@ -12,27 +11,32 @@ function App() {
   const nextPage = useRef();
 
   const photoRef = useCallback((element) => {
+    console.log("Photo ref mounter/unmounted");
+    const lastElementObserver = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        lastElementObserver.unobserve(entries[0].target);
+        if (nextPage.current) {
+          startPhotoFetch(nextPage.current);
+        }
+      }
+    });
     if (element == null) return;
     lastElementObserver.observe(element);
   }, []);
 
-  const lastElementObserver = new IntersectionObserver((entries) => {
-    if (entries[0].isIntersecting) {
-      lastElementObserver.unobserve(entries[0].target);
-      if (nextPage.current) {
-        startPhotoFetch(nextPage.current);
-      }
-    }
-  });
-
   const startPhotoFetch = async (url) => {
     setIsLoading(true);
-    const responseData = await fetch(url);
-    const data = await responseData.json();
-    const pagination = parseLinkHeader(responseData.headers.get("link"));
-    setPhotos((currPhotos) => [...currPhotos, ...data]);
-    nextPage.current = pagination.next;
-    setIsLoading(false);
+    try {
+      const responseData = await fetch(url);
+      const data = await responseData.json();
+      const pagination = parseLinkHeader(responseData.headers.get("link"));
+      setPhotos((currPhotos) => [...currPhotos, ...data]);
+      nextPage.current = pagination.next;
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   useEffect(() => {
@@ -40,7 +44,7 @@ function App() {
   }, []);
 
   return (
-    <div class="grid">
+    <div className="grid">
       {photos.map((photo, index) => {
         if (index == photos.length - 1) {
           return <img src={photo.url} key={photo.id} ref={photoRef} />;
