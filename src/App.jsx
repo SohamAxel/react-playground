c2c601512825d06105f4be536255d3753e33064b
Soham Sengupta <soham.sengupta@axelerant.com>
2024-05-28 16:15:41 +0530

    Project 2 setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,63 @@
-import { StateForm } from "./StateForm"
-import "./styles.css"
+import { StateForm } from "./StateForm";
+import "./styles.css";
+import { useLocalStorage } from "./useLocalStorage";
 
 export default function App() {
-  return <StateForm onSubmit={() => alert("Success")} />
+  const [firstName, setFirstName] = useLocalStorage("FIRST_NAME", "");
+
+  const [lastName, setLastName] = useLocalStorage("LAST_NAME", () => {
+    return "Default";
+  });
+
+  const [hobbies, setHobbies] = useLocalStorage("HOBBIES", [
+    "Programming",
+    "Weight Lifting",
+  ]);
+  return (
+    <>
+      <StateForm onSubmit={() => alert("Success")} />
+      <>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+            marginBottom: "1rem",
+          }}
+        >
+          <label>First Name</label>
+          <input
+            type="text"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+            marginBottom: "1rem",
+          }}
+        >
+          <label>Last Name</label>
+          <input
+            type="text"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </div>
+
+        <div>{hobbies.join(", ")}</div>
+        <button
+          onClick={() =>
+            setHobbies((currentHobbies) => [...currentHobbies, "New Hobby"])
+          }
+        >
+          Add Hobby
+        </button>
+      </>
+    </>
+  );
 }
